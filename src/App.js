a86708b4708efeea5fcd99c7dea0b29975777b17
chojaework <chojaework@gmail.com>
2023-07-14 20:28:03 +0900

    bitcoin proj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response => response.json())
+    .then((json) => console.log(json)))
+  }, [])
+  return <div>
+    <h1>The Coins!</h1>
+    {loading ? <strong>Loading...</strong> : null}
+  </div>
 }
 
 export default App;
