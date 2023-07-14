5e9a6d577dc3eeeaadbc3b0b97353590b1478afc
chojaework <chojaework@gmail.com>
2023-07-14 21:06:39 +0900

    7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response => response.json())
-    .then((json) => console.log(json)))
-  }, [])
-  return <div>
-    <h1>The Coins!</h1>
-    {loading ? <strong>Loading...</strong> : null}
-  </div>
+    fetch(
+      'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+      )
+    .then((response) => response.json())
+    .then((json) => {
+      setMovies(json.data.movies);
+      setLoading(false);
+    });
+  }, []);
+  console.log(movies);
+  return (<div> 
+      {loading ? (
+        <h1>Loading...</h1>
+        ) : (
+        <div>
+          {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image}/>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map(g => <li key={g}>{g}</li>)}
+            </ul>
+          </div>
+          ))}
+          </div>
+          )}
+    </div>
+  );
 }
 
 export default App;
